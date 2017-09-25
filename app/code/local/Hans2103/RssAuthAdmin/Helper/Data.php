faa145d695ccfc8fc794b0081ec0be50c868ab1f
Hans Kuijpers <hans2103@gmail.com>
2017-09-25 09:47:52 +0200

    apply Zend Conding Standard code styling.

--- app/code/local/Hans2103/RssAuthAdmin/Helper/Data.php
+++ app/code/local/Hans2103/RssAuthAdmin/Helper/Data.php
@@ -1,4 +1,4 @@
-<?php 
+<?php
 
 class Hans2103_RssAuthAdmin_Helper_Data extends Mage_Rss_Helper_Data
 {
@@ -9,26 +9,33 @@ class Hans2103_RssAuthAdmin_Helper_Data extends Mage_Rss_Helper_Data
      */
     public function authAdmin($path)
     {
-        if (!$this->_rssSession->isAdminLoggedIn() || !$this->_adminSession->isLoggedIn()) {
+        if (!$this->_rssSession->isAdminLoggedIn()
+            || !$this->_adminSession->isLoggedIn()
+        ) {
             list($username, $password) = $this->authValidate();
             Mage::getSingleton('adminhtml/url')->setNoSecret(true);
             $user = $this->_adminSession->login($username, $password);
         } else {
             $user = $this->_rssSession->getAdmin();
         }
-        if ($user && $user->getId() && $user->getIsActive() == '1' && $this->_adminSession->isAllowed($path)) {
-        	$adminUserExtra = $user->getExtra();
+        if ($user && $user->getId() && $user->getIsActive() == '1'
+            && $this->_adminSession->isAllowed($path)
+        ) {
+            $adminUserExtra = $user->getExtra();
 
             if ($adminUserExtra && !is_array($adminUserExtra)) {
                 //$adminUserExtra = Mage::helper('core/unserializeArray')->unserialize($user->getExtra());
-	            try {
-		            $adminUserExtra = Mage::helper('core/unserializeArray')->unserialize($user->getExtra());
-	            } catch (Exception $e) {
-		            $adminUserExtra = [];
-	            }
+                try {
+                    $adminUserExtra = Mage::helper('core/unserializeArray')
+                        ->unserialize($user->getExtra());
+                } catch (Exception $e) {
+                    $adminUserExtra = [];
+                }
             }
             if (!isset($adminUserExtra['indirect_login'])) {
-                $adminUserExtra = array_merge($adminUserExtra, array('indirect_login' => true));
+                $adminUserExtra = array_merge(
+                    $adminUserExtra, array('indirect_login' => true)
+                );
                 $user->saveExtra($adminUserExtra);
             }
             $this->_adminSession->setIndirectLogin(true);
